caaa66fcda801b7a6dfdb5f1cf78b819841eba06
shafq <shafqaness@gmail.com>
2024-06-26 12:04:20 +0500

    shafaq anees

---
+++ index.ts
@@ -0,0 +1,192 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let doContinue = true;
+
+console.log(chalk.cyan.italic("\n\t *Welcome to the Adventure Game*\t\n"));
+
+class Player{
+    name: string;
+    fuel: number = 100;
+
+    constructor(name: string){
+        this.name = name;
+    };
+    fuelDecrease(){
+        let fuel = this.fuel - 25
+        this.fuel = fuel
+    }
+    fuelIncrease(){
+        this.fuel = 100;
+    }
+
+};
+class Opponent {
+    name: string;
+    fuel: number = 100;
+
+    constructor(name: string){
+        this.name = name
+    }
+};
+
+let player = await inquirer.prompt({
+    type: "input",
+    name: "name",
+    message: chalk.yellowBright("Enter Player's Name:")
+});
+console.log(chalk.blue(player.name));
+
+let opponent = await inquirer.prompt({
+    type: "list",
+    name: "character",
+    message: chalk.red("Select your Opponent:"),
+    choices: [chalk.magentaBright("Dragonkin"), chalk.greenBright("Rubble Raider"), chalk.cyan("Titan Slayer")]
+});
+console.log(chalk.magenta(opponent.character));
+
+let p1 = new Player(player.name);
+let o1 = new Player(opponent.character);
+
+while(doContinue){
+if(opponent.character === chalk.magentaBright("Dragonkin")){
+
+let ask = await inquirer.prompt({
+    type: "list",
+    name: "action",
+    message: chalk.green("Select an Action:"),
+    choices: [chalk.red("Attack"), chalk.blue("Drink Portion"), chalk.yellow("Run For Your Life...")]
+});
+
+if(ask.action == chalk.red("Attack")){
+    let num = Math.floor(Math.random() * 2)
+
+    if(num > 0){
+        p1.fuelDecrease();
+        console.log(chalk.magentaBright(`\t${p1.name} fuel is ${p1.fuel}\t`));
+        console.log(chalk.yellowBright(`\t${o1.name} fuel is ${o1.fuel}\t`));
+        
+    if(p1.fuel <= 0){
+        console.log(chalk.red.italic("\tYou Lose! Better Luck Next Time...\t"));
+        process.exit();
+    }
+}
+
+    if(num <= 0){
+        o1.fuelDecrease();
+        console.log(chalk.green(`\t${p1.name} fuel is ${p1.fuel}\t`));
+        console.log(chalk.yellow(`\t${o1.name} fuel is ${o1.fuel}\t`));
+
+    if(o1.fuel <= 0){
+        console.log(chalk.cyan.italic("\tCongratulations! You Won!\t"));
+        process.exit();
+    }
+}
+};
+    if(ask.action == chalk.blue("Drink Portion")){
+        p1.fuelIncrease();
+        console.log(chalk.magentaBright.italic(`\tDrink Health Portion. Your fuel is ${p1.fuel}\t`));
+    }
+    if(ask.action == chalk.yellow("Run For Your Life...")){
+        console.log(chalk.red.italic("\tSorry! You Lose! Better Luck Next Time...\t"));
+    }
+};
+
+
+if(opponent.character === chalk.greenBright("Rubble Raider")){
+
+let ask = await inquirer.prompt({
+    type: "list",
+    name: "action",
+    message: chalk.green("Select an Action:"),
+    choices: [chalk.red("Attack"), chalk.blue("Drink Portion"), chalk.yellow("Run For Your Life...")]
+});
+
+if(ask.action == chalk.red("Attack")){
+    let num = Math.floor(Math.random() * 2)
+
+    if(num > 0){
+        p1.fuelDecrease();
+        console.log(chalk.magentaBright(`\t${p1.name} fuel is ${p1.fuel}\t`));
+        console.log(chalk.yellowBright(`\t${o1.name} fuel is ${o1.fuel}\t`));
+        
+    if(p1.fuel <= 0){
+        console.log(chalk.red.italic("\tYou Lose! Better Luck Next Time...\t"));
+        process.exit();
+    }
+};
+    if(num <= 0){
+        o1.fuelDecrease();
+        console.log(chalk.green(`\t${p1.name} fuel is ${p1.fuel}\t`));
+        console.log(chalk.yellow(`\t${o1.name} fuel is ${o1.fuel}\t`));
+
+    if(o1.fuel <= 0){
+        console.log(chalk.cyan.italic("\tCongratulations! You Won!\t"));
+        process.exit();
+    }
+}; 
+};
+if(ask.action == chalk.blue("Drink Portion")){
+    p1.fuelIncrease();
+    console.log(chalk.magentaBright.italic(`\tDrink Health Portion. Your fuel is ${p1.fuel}\t`));
+}
+if(ask.action == chalk.yellow("Run For Your Life...")){
+    console.log(chalk.red.italic("\tSorry! You Lose! Better Luck Next Time...\t"));
+
+};
+};
+if(opponent.character === chalk.cyan("Titan Slayer")){
+
+    let ask = await inquirer.prompt({
+        type: "list",
+        name: "action",
+        message: chalk.green("Select an Action:"),
+        choices: [chalk.red("Attack"), chalk.blue("Drink Portion"), chalk.yellow("Run For Your Life...")]
+    });
+if(ask.action == chalk.red("Attack")){
+        let num = Math.floor(Math.random() * 2)
+    
+    if(num > 0){
+            p1.fuelDecrease();
+            console.log(chalk.magentaBright(`\t${p1.name} fuel is ${p1.fuel}\t`));
+            console.log(chalk.yellowBright(`\t${o1.name} fuel is ${o1.fuel}\t`));
+            
+        if(p1.fuel <= 0){
+            console.log(chalk.red.italic("\tYou Lose! Better Luck Next Time...\t"));
+            process.exit();
+        }
+    }; 
+    if(num <= 0){
+        o1.fuelDecrease();
+        console.log(chalk.blue(`\t${p1.name} fuel is ${p1.fuel}\t`));
+        console.log(chalk.yellow(`\t${o1.name} fuel is ${o1.fuel}\t`));
+
+        if(o1.fuel <= 0){
+        console.log(chalk.cyan.italic("\tCongratulations Champion! You Won!\t"));
+        process.exit();
+    }
+}; 
+};
+if(ask.action == chalk.blue("Drink Portion")){
+    p1.fuelIncrease();
+    console.log(chalk.magentaBright.italic(`\tDrink Health Portion. Your fuel is ${p1.fuel}\t`));
+}
+if(ask.action == chalk.yellow("Run For Your Life And safe youself...")){
+    console.log(chalk.red.italic("\tSorry! You Lose! Good Luck Next Time...\t"));
+
+};
+
+};
+
+const startAgain = await inquirer.prompt(
+        {
+            type: "confirm",
+            name: "continue",
+            message: chalk.cyan("\nDo you want to Continue?Retry? \n"),
+            default: false
+        }
+    );  doContinue = startAgain.continue;
+    
+};
